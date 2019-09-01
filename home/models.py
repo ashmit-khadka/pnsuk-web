b030e83ac630c176b6909264b9c455981f346b21
Ash <wck17bhu@uea.ac.uk>
2019-09-01 19:32:03 +0100

    Big patch sept

--- home/models.py
+++ home/models.py
@@ -3,10 +3,15 @@ from django.core.validators import MaxLengthValidator
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='article_imgs', blank=True)
+    picture = models.ImageField(upload_to='article_imgs', blank=False, default='default/default.jfif')
     date = models.DateField() 
     text = models.CharField(max_length=10000)
     home = models.BooleanField(default=False)
+    news = models.BooleanField(default=True)
+    event = models.BooleanField(default=True)
+    guest = models.BooleanField(default=False)
+    project = models.BooleanField(default=False)
+
 
 class Event(models.Model):
     name = models.CharField(max_length=100)
@@ -21,21 +26,26 @@ class Policy(models.Model):
  
 class Project(models.Model):
     title = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='project_imgs', blank=True)
+    picture = models.ImageField(upload_to='project_imgs', blank=False, default='default/default.jfif')
     date = models.DateField() 
     text = models.CharField(max_length=10000)
     home = models.BooleanField(default=False)
+    news = models.BooleanField(default=False)
+
 
 class Event_Past(models.Model):
     title = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='event_imgs', blank=True)
+    picture = models.ImageField(upload_to='event_imgs', blank=False, default='default/default.jfif')
     date = models.DateField() 
     text = models.CharField(max_length=10000)
     home = models.BooleanField(default=False)
+    news = models.BooleanField(default=False)
 
 class Guest(models.Model):
     title = models.CharField(max_length=100)
-    picture = models.ImageField(upload_to='guestt_imgs', blank=True)
+    picture = models.ImageField(upload_to='guestt_imgs', blank=False, default='default/default.jfif')
     date = models.DateField() 
     text = models.CharField(max_length=10000)
     home = models.BooleanField(default=False)
+    news = models.BooleanField(default=False)
+
